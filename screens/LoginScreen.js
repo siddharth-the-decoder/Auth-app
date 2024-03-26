ae86cec17ddaad15c69c4beac865aaf12661bfe6
Siddharth Tripathi <tripathisiddharth058@gmail.com>
2024-03-26 21:38:30 +0530

    login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,26 @@
-import AuthContent from '../components/Auth/AuthContent';
-
+import AuthContent from "../components/Auth/AuthContent";
+import { useState } from "react";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthentiating, setIsAuthenticating] = useState(false);
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+      await login(email, password);
+    } catch (error) {
+      console.log(error);
+      Alert.alert
+    }
+
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthentiating) {
+    return <LoadingOverlay message="Logging you in..." />;
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
